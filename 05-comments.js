48e5f58fca31636dedba7dacb4e97ad898c328ef
chandlertai <97628440+chandlertai@users.noreply.github.com>
2022-03-30 17:59:14 -0400

    comments

---
+++ 05-comments.js
@@ -0,0 +1,11 @@
+// Single line comment
+/* 
+Mulitline comments
+*/
+// Comments are usedmto communicate with yourself in the future or other developers.
+// Comments add readability to the code by letting you writenin plain english.
+// Comments can allow ou to debug by excluding certain lines of code.
+
+// Shorthand: Windowss CTRL + / and Macs: CMD + /
+
+// JavaScript is a programming interpretted language (It just means that you can give instructions to your computer and it can handle them) That was dev eloped in 1995 by Brenden Eich of Netscapes. We are on version ECMAScript 7
---
+++ 06-arrays.js
@@ -0,0 +1,7 @@
+let rainbowColors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'violet'];
+let raceWinners = [33,72,64];
+let myFavoriteThings = ['Broccoli', 46074, 'Zombie Cats From Mars'];
+
+var mySecretSpyInfo = ['James Bond', 007, true]; // A string, a number (although the 00 is ignored), and a boolean
+
+var country
