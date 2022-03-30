2a45489ed34c3837fa5d826c863b0464b0ccada4
chandlertai <97628440+chandlertai@users.noreply.github.com>
2022-03-30 17:27:51 -0400

    objects

---
+++ 07-objects.js
@@ -0,0 +1,22 @@
+// 1       2
+let zelda = {
+    //3         //4
+    name       :"Zelda",
+    age        : 17,
+    title      : "Princess",
+    isCaptured : true
+};
+
+
+// dot (.) is an operator
+// ObjectsName.propertyName
+
+console.log(zelda);  //1 
+console.log(zelda.name);  //2
+console.log(zelda.age);  //3
+
+// 1. Prints the whole object.
+// 2. Prints the value of the name property.
+// 3. Prints the value of the age property.
+
+console.log(zelda.middleName); // undefined: middleName does not exist
