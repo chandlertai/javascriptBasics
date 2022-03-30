4b34463f4d8f4fb75dbcfd8c2f4c2785b90fd24a
chandlertai <97628440+chandlertai@users.noreply.github.com>
2022-03-30 18:31:41 -0400

    variables2

--- 01-variables.js
+++ 01-variables.js
@@ -1,3 +1,17 @@
+// VAriables are containers for storing information
+// Variables are named so we can refer to them later and put values into them
+
+/*
+ let nameOfVariable = value;
+
+  (1)           (2)   (3)   (4)
+
+ 1. let is a variable
+ 2. name of the variable
+ 3. Assignment operator
+ 4. out actual value that variable holds
+*/
+
 /*
 Rules for naming Variables:
 - Name must begin with a letter, dollar sign $, or underscore_
@@ -6,6 +20,11 @@ Rules for naming Variables:
 - Names should be descriptive and  brief, for ex: isTrue; enviromentStatus; ect. There are other best practices when it comes to nomenclature.
 */
 
+let firstName = "Elaina";
+console.log(firstName);
+
+
+
 //Declaration
 
 var patientId;
@@ -13,4 +32,11 @@ var patientId;
 // Initialization
 
 patientId = 120023;
+
+/*
+3 different keywords to declare a variable
+1. let = is the standard keyword. we can reinitialize and change our value with this.
+2. var = old way of declaring a variable
+3. const = Value can never change, Constant
+*/
 
