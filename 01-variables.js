4a946fae4012d052ff18d580ce6c127442ad2b86
chandlertai <97628440+chandlertai@users.noreply.github.com>
2022-03-30 14:12:10 -0400

    Initial commit

---
+++ 01-variables.js
@@ -0,0 +1,16 @@
+/*
+Rules for naming Variables:
+- Name must begin with a letter, dollar sign $, or underscore_
+- Name must not begin with a number
+- Name cannot b e reserved words. These are variables already utilized by Javascript itself.
+- Names should be descriptive and  brief, for ex: isTrue; enviromentStatus; ect. There are other best practices when it comes to nomenclature.
+*/
+
+//Declaration
+
+var patientId;
+
+// Initialization
+
+patientId = 120023;
+
