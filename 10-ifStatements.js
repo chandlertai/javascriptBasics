6fc8a6d21960fe0fd9fdab313a3915eb539363c1
chandlertai <97628440+chandlertai@users.noreply.github.com>
2022-03-30 21:03:46 -0400

    ifStatements

---
+++ 10-ifStatements.js
@@ -0,0 +1,16 @@
+// 1   //2
+if (condition) { //3
+// block of code to be executed if the condition is true
+};
+
+//1. Any if statement must start with the if keyword
+//2. The condition that is being checked is inside the parenthesis
+//3. If the condition elvaluates to true the code inside the curlu bois will execute. If it evaluates to false the code will not run.
+
+if (10 > 5) {
+    console.log("Yep, 10 is greater than 5.");
+};
+
+if (10 < 5) {
+    console.log("This code will not run! 10 is not greater than 5.");
+};
