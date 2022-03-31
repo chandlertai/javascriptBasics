6a5e5aedfd015b503afbbe2f5fdc3a3b6910f1f5
chandlertai <97628440+chandlertai@users.noreply.github.com>
2022-03-31 14:50:33 -0400

    ternaries

---
+++ 14-ternaries.js
@@ -0,0 +1,9 @@
+//1                //2                //3
+( condition ) ? run this code : run this code instead
+
+//1. This code is the condition that your testing.
+//2. After the condition we add a ? and then the code we want to execute if it is true.
+//3. Next we add the : and the code we want to run if it is false.
+
+let cookies = 5;
+(cookies > 0) ? console.log("I'm going to eat a cookie!") : console.log("I have no cookies :(")
