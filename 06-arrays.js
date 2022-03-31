76528e8eda7cac93a0e844fe07be38589819d432
chandlertai <97628440+chandlertai@users.noreply.github.com>
2022-03-30 20:33:37 -0400

    arrays

--- 06-arrays.js
+++ 06-arrays.js
@@ -4,4 +4,16 @@ let myFavoriteThings = ['Broccoli', 46074, 'Zombie Cats From Mars'];
 
 var mySecretSpyInfo = ['James Bond', 007, true]; // A string, a number (although the 00 is ignored), and a boolean
 
-var country
+
+var countryArray = ["USA", "Japan", "Belgium"];
+console.log(countryArray[2]);// Belgium
+console.log(countryArray[0]);// USA
+
+let dogs = ["Shiba Inu", "husky", "corgi", "Great Dane"];
+let cats = ["Maine Coon", "bengal", "Siamese", "Sphynx"];
+let birds = ["African Grey Parrot", "Canary", "Macaw", "Finch"];
+let fish = ["Goldfish", "Neon Tetra", "Betta", "Guppy"];
+console.log(dogs[0]);
+console.log(cats[1]);
+console.log(fish[3]);
+console.log(birds[0]);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let dogs = ["Shiba Inu", "husky", "corgi", "Great Dane"];
+let cats = ["Maine Coon", "bengal", "Siamese", "Sphynx"];
+let birds = ["African Grey Parrot", "Canary", "Macaw", "Finch"];
+let fish = ["Goldfish", "Neon Tetra", "Betta", "Guppy"];
+console.log(dogs[0]);
+console.log(cats[1]);
+console.log(fish[3]);
+console.log(birds[0]);
