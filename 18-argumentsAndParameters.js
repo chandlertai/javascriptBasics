91e25d3b9e0dd140f97504c3a6009f1f98befac6
chandlertai <97628440+chandlertai@users.noreply.github.com>
2022-04-03 14:19:48 -0400

    aandP

---
+++ 18-argumentsAndParameters.js
@@ -0,0 +1,15 @@
+// functions can also accept values known as arguments if the correct parameters are set up
+
+function numberEntered(num){
+    console.log(`The number you entered was: ${num}`);
+}
+
+
+
+//Declare it
+function subtractTwoNumbers(firstNum, secondNum){
+    console.log(firstnum - secondNum);
+}
+
+//Call it
+subtractTwoNumbers(5, 7);
